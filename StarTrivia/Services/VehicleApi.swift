8a6c630c6508a41191b8ede06ec46cdaef4feb18
Juan Carlos Chávez <juancho989@gmail.com>
2019-05-09 11:37:27 -0500

    Added Vehicle model and VehicleApi class.

---
+++ StarTrivia/Services/VehicleApi.swift
@@ -0,0 +1,35 @@
+//
+//  VehicleApi.swift
+//  StarTrivia
+//
+//  Created by Juan Chavez Montes on 5/9/19.
+//  Copyright © 2019 Juan Chavez Montes. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+
+class VehicleApi {
+    
+    func getVehicle(url: String, completion: @escaping VehicleResponseCompletion) {
+        guard let url = URL(string: url) else { return }
+        Alamofire.request(url).responseJSON { (response) in
+            if let error = response.result.error {
+                debugPrint(error.localizedDescription)
+                completion(nil)
+                return
+            }
+            
+            guard let data = response.data else { return completion(nil) }
+            let jsonDecoder = JSONDecoder()
+            do {
+                let vehicle = try jsonDecoder.decode(Vehicle.self, from: data)
+                completion(vehicle)
+            } catch {
+                debugPrint(error.localizedDescription)
+                completion(nil)
+            }
+        }
+    }
+    
+}
